afe11f93c92b21cdf7d38d5f13e4a2366b83b790
nam.le <nam.le@2359media.com>
2022-07-11 23:52:16 +0700

    feat: add load more page

--- pages/component/movie/ListMovie.tsx
+++ pages/component/movie/ListMovie.tsx
@@ -1,9 +1,21 @@
 import { useEffect, useState } from 'react';
 import { request } from '../../../utils/request';
 import Movie from './Movie';
-
+import styled from 'styled-components';
+const Style = styled.div`
+  .loadmore {
+    margin-top: 20px;
+    button {
+      background-color: #6a5af9;
+      color: white;
+      margin-bottom: 20px;
+    }
+    text-align: center;
+  }
+`;
 const ListMovie = () => {
   const [loading, setLoading] = useState(true);
+  const [loadMore, setLoadMore] = useState(false);
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     if (loading) {
@@ -14,17 +26,35 @@ const ListMovie = () => {
         });
         setMovies(res.data);
         setLoading(false);
+        if (res.data.length < res.count) {
+          setLoadMore(true);
+        }
       };
       fetchPost();
     }
   }, []);
 
+  const onClickLoadMore = async () => {
+    const res: any = await request({
+      path: `/movies`,
+      method: 'GET',
+    });
+    setMovies([...movies, ...res.data]);
+    if (res.data.length < res.count) {
+      setLoadMore(true);
+    } else {
+      setLoadMore(false);
+    }
+  };
   return (
-    <div>
+    <Style>
       {movies.map(movie => (
         <Movie key={movie.id} movie={movie}></Movie>
       ))}
-    </div>
+      <div className="loadmore">
+        <button onClick={onClickLoadMore}>Load more</button>
+      </div>
+    </Style>
   );
 };
 export default ListMovie;
